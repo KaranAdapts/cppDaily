7deab48d8cf186c904f3e9b01ca5af77d6540888
Karan <karanphadtare15@gmail.com>
2026-04-20 23:56:08 +0530

    Learnt how to pass a Function's return value as an argument directly

---
+++ learncpp/Chapter_2/funcIsArg.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+
+// You can pass arguments by the return value of another function directly 
+
+int thisNum(){
+    std::cout << "Enter your number to be doubled: " << '\n';
+    int input{}; 
+    std::cin >> input; 
+
+    return input;
+}
+
+void doubleNum(int value){
+    std::cout << value << " doubled is " << value * 2 << '\n';
+}
+
+int main() {
+    doubleNum( thisNum()); // initalized doubleNum by the input value of thisNum() itself no need for another variable
+}
