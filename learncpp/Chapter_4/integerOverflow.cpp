a142744f6770532977b1197b05ddbcea7143ad8d
Karan <karanphadtare15@gmail.com>
2026-05-10 00:11:54 +0530

    Demonstration of Integer Overflow and undefined behaviour caused by it

--- learncpp/Chapter_4/integerOverflow.cpp
+++ learncpp/Chapter_4/integerOverflow.cpp
@@ -1,10 +1,11 @@
 #include<iostream>
-#include<iomanip>
+#include<iomanip> // this header file is used for setting width of the output/purely formatting purposes
 
 int main(){
 
-    int exceedMax{2'147'483'649};
-    std::cout << std::setw(16) << "Integer Value is: " << exceedMax << " bytes" << '\n';
+    int exceedMax{2'147'483'647}; // an int can hold values from -2,147,483,648 to 2,147,483,647, getting it out of range results in undefined behaviour
+    exceedMax = exceedMax + 1; 
+    std::cout << "Integer Overflow: " << std::setw(16) << exceedMax << " integer value" << '\n';
 
     return 0;
 }
