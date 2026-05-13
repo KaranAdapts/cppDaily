0db6a6a1670f7a8b0b862ce66eedf65b98d5ec10
Karan <karanphadtare.dev@gmail.com>
2026-05-13 23:44:45 +0530

    Checking Prime Numbers from 0-9: Learnt how to use if-else statements with OR operations to help with readability

---
+++ learncpp/Chapter_4/quizTime.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+
+int takeInput(){
+
+    std::cout << "Enter an Integer (Range 0 - 9): "; 
+    int inputVal{};
+    std::cin >> inputVal; 
+
+    return inputVal;
+}
+
+int main(){
+
+    int userInput{takeInput()};
+
+    if (userInput > 9 || userInput < 0) {
+        std::cout << "Out of range";
+    }
+    else if (userInput == 2 || userInput ==  3 || userInput ==  5 || userInput == 7){
+        std::cout << "The digit is prime";
+    }
+    else {
+        std::cout << "The digit is not a prime";
+    }
+
+
+    return 0;
+}
